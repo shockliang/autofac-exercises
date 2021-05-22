748a5587c643d832a46436e3e538b2fa4e3c6537
shockliang <shockwater@gmail.com>
2021-05-22 15:30:45 +0800

    Initial project for scenario without di.

---
+++ WithoutDI/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace WithoutDI
+{
+    class Program
+    {
+        public interface ILog
+        {
+            void Write(string message);
+        }
+        
+        public class ConsoleLog: ILog
+        {
+            public void Write(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        public class Engine
+        {
+            private ILog logger;
+            private int id;
+
+            public Engine(ILog logger)
+            {
+                this.logger = logger;
+                id = new Random().Next();
+            }
+
+            public void Ahead(int power)
+            {
+                logger.Write($"Engine [{id}] ahead {power}");
+            }
+            
+        }
+
+        public class Car
+        {
+            private ILog logger;
+            private Engine engine;
+
+            public Car(Engine engine, ILog logger)
+            {
+                this.engine = engine;
+                this.logger = logger;
+            }
+
+            public void Go()
+            {
+                engine.Ahead(100);
+                logger.Write($"Car going forward");
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            var logger = new ConsoleLog();
+            var engine = new Engine(logger);
+            var car = new Car(engine, logger);
+            car.Go();
+        }
+    }
+}
