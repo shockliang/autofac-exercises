309910e2ba7b60775dd17e952c1ca4872010aeab
shockliang <shockwater@gmail.com>
2021-05-31 00:25:35 +0800

    Property and method injection.

--- BaseScenario/Program.cs
+++ BaseScenario/Program.cs
@@ -154,19 +154,49 @@ namespace BaseScenario
                 Console.WriteLine(entity);
             }
         }
+        
+        class Parent
+        {
+            public override string ToString()
+            {
+                return "I am your father";
+            }
+        }
 
+        class Child
+        {
+            public string Name { get; set; }
+            public Parent Parent { get; set; }
+
+            public void SetParent(Parent parent)
+            {
+                Parent = parent;
+            }
+            
+        }
         
         static void Main(string[] args)
         {
             var builder = new ContainerBuilder();
-            builder.RegisterType<Entity>().InstancePerDependency();
-            builder.RegisterType<ViewModel>();
+            builder.RegisterType<Parent>();
+            // builder.RegisterType<Child>().PropertiesAutowired();
+            // builder.RegisterType<Child>().WithProperty("Parent", new Parent());
+            // builder.Register(c =>
+            // {
+            //     var child = new Child();
+            //     child.SetParent(c.Resolve<Parent>());
+            //     return child;
+            // });
 
+            builder.RegisterType<Child>().OnActivated(e =>
+            {
+                var p = e.Context.Resolve<Parent>();
+                e.Instance.SetParent(p);
+            });
+            
             var container = builder.Build();
-            var viewModel = container.Resolve<ViewModel>();
-            viewModel.SomeMethod();
-            viewModel.SomeMethod();
-
+            var parent = container.Resolve<Child>().Parent;
+            Console.WriteLine(parent);
         }
     }
 }
