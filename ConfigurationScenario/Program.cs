8f349bb28cc7504f3039de99e110aca55b702aea
shockliang <shockwater@gmail.com>
2021-06-16 17:57:59 +0800

    Json configuration.

--- ConfigurationScenario/Program.cs
+++ ConfigurationScenario/Program.cs
@@ -1,17 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using Autofac;
+using Autofac.Configuration;
+using Microsoft.Extensions.Configuration;
 
 namespace ConfigurationScenario
 {
+    public interface IOperation
+    {
+        float Calculate(float a, float b);
+    }
+
+    public class Addition: IOperation
+    {
+        public float Calculate(float a, float b)
+        {
+            return a + b;
+        }
+    }
+
+    public class Multiplication: IOperation
+    {
+        public float Calculate(float a, float b)
+        {
+            return a * b;
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            var builder = new ContainerBuilder();
-            builder.RegisterModule(new TransportModule {ObeySpeedLimit = true});
-            using (var c = builder.Build())
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json");
+            var configuration = configBuilder.Build();
+            var containerBuilder = new ContainerBuilder();
+            var configModule = new ConfigurationModule(configuration);
+            containerBuilder.RegisterModule(configModule);
+
+            using (var container = containerBuilder.Build())
             {
-                c.Resolve<IVehicle>().Go();
+                float a = 3, b = 4;
+                foreach (var op in container.Resolve<IList<IOperation>>())
+                {
+                    Console.WriteLine($"{op.GetType().Name} of {a} and {b} = {op.Calculate(a, b)}");
+                }
             }
         }
     }
