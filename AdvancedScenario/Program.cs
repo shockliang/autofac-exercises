a653b4a9572d5ba2c09733cd190f1cffe50f3ad1
shockliang <shockwater@gmail.com>
2021-06-19 14:06:02 +0800

    Registration sources.

---
+++ AdvancedScenario/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Autofac;
+using Autofac.Features.ResolveAnything;
+
+namespace AdvancedScenario
+{
+    class Program
+    {
+        public interface ICanSpeak
+        {
+            void Speak();
+        }
+
+        public class Person : ICanSpeak
+        {
+            public void Speak()
+            {
+                Console.WriteLine("HELLO!");
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterSource(new AnyConcreteTypeNotAlreadyRegisteredSource());
+            
+            using var container = builder.Build();
+            container.Resolve<Person>().Speak();
+
+        }
+    }
+}
